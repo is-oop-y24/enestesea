0251997bf7daaf5da5ed3b885eb2c1f74fbf0b28
настя <75230747+enestesea@users.noreply.github.com>
2021-09-20 02:17:23 +0300

    Update IsuServiceTest.cs

--- Isu.Tests/IsuServiceTest.cs
+++ Isu.Tests/IsuServiceTest.cs
@@ -51,11 +51,21 @@ namespace Isu.Tests
         {
             Assert.Catch<IsuException>(() =>
             {
-                Group group1 = _isuService.AddGroup("M3200");
-                Group group2 = _isuService.AddGroup("M3204");
-                Student student1 = _isuService.AddStudent(group1, "Valeriy Zhmushenko");
+                Group group1 = _isuService.AddGroup("M3204");
+                Group group2 = _isuService.AddGroup("M3200");
+                for (int i = 0; i < 33; i++)
+                { 
+                    _isuService.AddStudent(group2, "Valeriy Zhmushenko");
+                }
+                Student student1 = _isuService.AddStudent(group1, "Denis Ivanov");
                 _isuService.ChangeStudentGroup(student1, group2);
             });
+            Group group1 = _isuService.AddGroup("M3202");
+            Group group2 = _isuService.AddGroup("M3203");
+            Student student1 = _isuService.AddStudent(group1, "Valya Villo");
+            _isuService.ChangeStudentGroup(student1, group2);
+            Assert.AreEqual(student1.GroupName, group2.GroupName);
+            Assert.Contains(student1, group2.Students);
         }
     }
 }

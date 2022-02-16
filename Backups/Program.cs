c2be0a2593cfb2ade48604d1a590a65aeb02b1b4
настя <75230747+enestesea@users.noreply.github.com>
2022-02-16 20:43:56 +0300

    Update Program.cs

--- Backups/Program.cs
+++ Backups/Program.cs
@@ -39,6 +39,28 @@ namespace Backups
             backupJob.CreateRestorePoint();
             Console.WriteLine(backupJob.JobObjectsNumber);
             Console.WriteLine(backupJob.RestorePointsNumber);
+            IWayOfStorage wayOfStorage2 = new SingleStorage();
+            List<IRepository> listOfRepositories2 = new List<IRepository>();
+            Directory.CreateDirectory(@"C:/BackupDirectory2");
+            IRepository local2 = new LocalRepository(@"C:/BackupDirectory2");
+            listOfRepositories.Add(local2);
+            BackupsJob backupJob2 = new BackupsJob(listOfRepositories2, wayOfStorage2);
+            using (FileStream fileStream = File.Create(@"C:/BackupDirectory2/File1.docx"))
+            {
+                byte[] information = new UTF8Encoding(true).GetBytes("Test1");
+                fileStream.Write(information, 0, information.Length);
+            }
+
+            using (FileStream fileStream = File.Create(@"C:/BackupDirectory2/File2.docx"))
+            {
+                byte[] information = new UTF8Encoding(true).GetBytes("Test2");
+                fileStream.Write(information, 0, information.Length);
+            }
+
+            backupJob2.AddJobObject(@"C:/BackupDirectory2/File1.docx");
+            backupJob2.AddJobObject(@"C:/BackupDirectory2/File2.docx");
+            backupJob2.CreateRestorePoint();
+            Console.WriteLine(Directory.Exists(@"C:/BackupDirectory2"));
         }
     }
 }
